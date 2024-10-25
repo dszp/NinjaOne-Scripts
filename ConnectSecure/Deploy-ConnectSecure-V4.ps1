5c48c4012fc8b397840d03e65f71751043546eb6
David Szpunar <dszp@users.noreply.github.com>
2024-10-25 12:15:52 -0400

    Add additional error checking for User Secret, add custom field configuration for it, and add hardcoded override for User Secret as an option

--- ConnectSecure/Deploy-ConnectSecure-V4.ps1
+++ ConnectSecure/Deploy-ConnectSecure-V4.ps1
@@ -48,6 +48,7 @@ Version 0.1.3 - 2023-11-13 - Fix logic bug in new Script Variables handling meth
 Version 0.2.0 - 2023-12-07 - Update to support ConnectSecure v4 beta and removing the v3 agent if it exists
 Version 0.2.1 - 2024-03-28 - Add a different supported TLS version check before download to attempt and fix
 Version 0.2.2 - 2024-10-25 - Add support for new -j, user secret, parameter described here -  https://cybercns.atlassian.net/wiki/spaces/CVB/pages/2111242438/How+To+Install+V4+Agent+Using+RMM+Script
+Version 0.2.3 - 2024-10-25 - Add additional error checking for User Secret, add custom field configuration for it, and add hardcoded override for User Secret as an option
 
 NOTE: This script and all options have not been fully and exhaustively tested with ConnectSecure's latest v4 release, though installation 
 seems to be working fine with light testing. Due to substantial changes since v4's original release to today's beta version, 
@@ -62,9 +63,9 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #>
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory=$false)][int] $CompanyID = $env:companyid,
-    [Parameter(Mandatory=$false)][string] $TenantID = $env:tenentid,
-    [Parameter(Mandatory=$false)][string] $userSecret = $env:userSecret,
+    [Parameter(Mandatory = $false)][int] $CompanyID = $env:companyid,
+    [Parameter(Mandatory = $false)][string] $TenantID = $env:tenentid,
+    [Parameter(Mandatory = $false)][string] $userSecret = $env:userSecret,
     [switch] $Once,
     [switch] $Force,
     [switch] $UninstallPrevious,
@@ -74,16 +75,19 @@ param(
 ### PROCESS NINJRAMM SCRIPT VARIABLES AND ASSIGN TO NAMED SWITCH PARAMETERS
 # Get all named parameters and overwrite with any matching Script Variables with value of 'true' from environment variables
 # Otherwise, if not a checkbox ('true' string), assign any other Script Variables provided to matching named parameters
-$switchParameters = (Get-Command -Name $MyInvocation.InvocationName).Parameters;
+$switchParameters = (Get-Command -Name $MyInvocation.InvocationName).Parameters
 foreach ($param in $switchParameters.keys) {
-    $var = Get-Variable -Name $param -ErrorAction SilentlyContinue;
-    if($var) {
+    $var = Get-Variable -Name $param -ErrorAction SilentlyContinue
+    if ($var) {
         $envVarName = $var.Name.ToLower()
         $envVarValue = [System.Environment]::GetEnvironmentVariable("$envVarName")
-        if (![string]::IsNullOrWhiteSpace($envVarValue) -and ![string]::IsNullOrEmpty($envVarValue) -and $envVarValue.ToLower() -eq 'true') {    # Checkbox variables
+        if (![string]::IsNullOrWhiteSpace($envVarValue) -and ![string]::IsNullOrEmpty($envVarValue) -and $envVarValue.ToLower() -eq 'true') {
+            # Checkbox variables
             $PSBoundParameters[$envVarName] = $true
             Set-Variable -Name "$envVarName" -Value $true -Scope Script
-        } elseif (![string]::IsNullOrWhiteSpace($envVarValue) -and ![string]::IsNullOrEmpty($envVarValue) -and $envVarValue -ne 'false') {       # non-Checkbox string variables
+        }
+        elseif (![string]::IsNullOrWhiteSpace($envVarValue) -and ![string]::IsNullOrEmpty($envVarValue) -and $envVarValue -ne 'false') {
+            # non-Checkbox string variables
             $PSBoundParameters[$envVarName] = $envVarValue
             Set-Variable -Name "$envVarName" -Value $envVarValue -Scope Script
         }
@@ -92,21 +96,26 @@ foreach ($param in $switchParameters.keys) {
 ### END PROCESS SCRIPT VARIABLES
 
 ##### CONFIG
-# Manually set the global Tenant ID for CyberCNS V4 instance, if not passed in via variable or parameter already:
-if([string]::IsNullOrEmpty($TenantID) -or [string]::IsNullOrWhiteSpace($TenantID)) {
+# Manually set the global Tenant ID for ConnectSecure V4 instance, if not passed in via variable or parameter already:
+if ([string]::IsNullOrEmpty($TenantID) -or [string]::IsNullOrWhiteSpace($TenantID)) {
     $TenantID = '000000000000000000'    # Numeric Tenant ID string from the deployment script
 }
 
-
-
+# Manually set the Script Parameter, Script Variable, or NinjaOne Custom Field or Documentation Custom Field instead of being hardcoded
+if ([string]::IsNullOrEmpty($userSecret) -or [string]::IsNullOrWhiteSpace($userSecret)) {
+    $userSecret = ''    # User Secret optionally hardcoded to a specific user value (the -j parameter from the install string)
+}
 
 # The name of your NinjaRMM Documentation document that contains the custom field below, assumes that there is both a template 
 # and a single instance of the document with the same name (otheriwse change to Ninja-Property-Docs-Get calls below with separate 
 # template and instance names):
 $CustomNinjaDocumentation = 'Deployments'
 
-# NinjaRMM Custom Documentation Field Name (third can be secure, all must have Script Read permissions)
+# NinjaRMM Custom Documentation Field Name (can be secure, all must have Script Read permissions)
 $customCompanyID = 'connectsecureCompanyId'
+
+# NinjaRMM Custom Documentation Field Name (can be secure, all must have Script Read permissions)
+$customUserSecret = 'connectsecureUserSecret'
 
 $InstallLocation = $env:TEMP        # Temporary downloaded installer location (can leave as temp folder)
 $InstallFilename = 'cybercnsagent'  # Temporary downloaded installer filename (can leave as-is)
@@ -130,7 +139,7 @@ if (-not (Test-IsElevated)) {
 }
 
 ##### UNINSTALL/REMOVAL of VERSION 4 AGENT
-if($Uninstall) {
+if ($Uninstall) {
     Write-Host "Uninstalling ConnectSecure CyberCNS Lightweight Agent..."
     If ( (Get-Service $ServiceName -ErrorAction SilentlyContinue) ) {
         Write-Host "The service" $ServiceName "is installed. Uninstalling."
@@ -138,14 +147,15 @@ if($Uninstall) {
 
     # Run Uninstall Batch Script If It Exists
     $UninstallReturnValue = 0
-    $uninstallBatPaths = @("${Env:ProgramFiles(x86)}\CyberCNSAgent\uninstall.bat","$Env:Programfiles\CyberCNSAgent\uninstall.bat")
+    $uninstallBatPaths = @("${Env:ProgramFiles(x86)}\CyberCNSAgent\uninstall.bat", "$Env:Programfiles\CyberCNSAgent\uninstall.bat")
     $uninstallBatPath = "${Env:ProgramFiles(x86)}\CyberCNSAgent\uninstall.bat"
     foreach ($path in $uninstallBatPaths) {
         Write-Host "Checking if path exists: $path"
         if (Test-Path $path) {
             $uninstallBatPath = $path
             Write-Host "(it does)"
-        } else {
+        }
+        else {
             Write-Host "(it does not)"
         }
     }
@@ -172,25 +182,29 @@ rmdir CyberCNSAgent /s /q
     if (Test-Path $uninstallBatPath) {
         Write-Host "Uninstall batch script exists. Running it..."
         $UninstallReturnValue = Start-Process -FilePath $uninstallBatPath -Wait
-        if(Test-Path $uninstallBatPath) {
+        if (Test-Path $uninstallBatPath) {
             Write-Host "Uninstall batch script still exists after removal attempt, you may wish to verify or manually remove."
         }
         Write-Host "Uninstall batch script removal attempt completed."
         If ( (Get-Service $ServiceName -ErrorAction SilentlyContinue) ) {
             Write-Host "The service" $ServiceName "is still installed."
-        } else {
+        }
+        else {
             Write-Host "The service" $ServiceName "is confirmed to not exist."
         }
-    } elseif ($Force) {
+    }
+    elseif ($Force) {
         Write-Host "Uninstall batch script does not exist and -Force flag is set. Running alternative cleanup script..."
         $UninstallReturnValue = Start-Process -FilePath "cmd.exe" -ArgumentList "/c $alternativeScript" -Wait
         Write-Host "Alternative cleanup script removal attempt completed."
         If ( (Get-Service $ServiceName -ErrorAction SilentlyContinue) ) {
             Write-Host "The service" $ServiceName "is still installed."
-        } else {
+        }
+        else {
             Write-Host "The service" $ServiceName "is confirmed to not exist."
         }
-    } else {
+    }
+    else {
         Write-Host "Uninstall batch script does not exist, nothing else done. To try built-in removal anyway, re-run adding -Force flag."
     }
 
@@ -205,7 +219,7 @@ rmdir CyberCNSAgent /s /q
 If ( (Get-Service $ServiceNameV3 -ErrorAction SilentlyContinue) ) {
     $UninstallPrevious = $true
 }
-if($UninstallPrevious) {
+if ($UninstallPrevious) {
     Write-Host "Uninstalling ConnectSecure CyberCNS Lightweight Agent from Version 3..."
     If ( (Get-Service $ServiceNameV3 -ErrorAction SilentlyContinue) ) {
         Write-Host "The service" $ServiceNameV3 "is installed (v3 agent) or force-remove is requested. Uninstalling."
@@ -213,14 +227,15 @@ if($UninstallPrevious) {
 
     # Run Uninstall Batch Script If It Exists
     $UninstallReturnValue = 0
-    $uninstallBatPaths = @("${Env:ProgramFiles(x86)}\CyberCNSAgentV2\uninstall.bat","$Env:Programfiles\CyberCNSAgentV2\uninstall.bat")
+    $uninstallBatPaths = @("${Env:ProgramFiles(x86)}\CyberCNSAgentV2\uninstall.bat", "$Env:Programfiles\CyberCNSAgentV2\uninstall.bat")
     $uninstallBatPath = "${Env:ProgramFiles(x86)}\CyberCNSAgentV2\uninstall.bat"
     foreach ($path in $uninstallBatPaths) {
         Write-Host "Checking if path exists: $path"
         if (Test-Path $path) {
             $uninstallBatPath = $path
             Write-Host "(it does)"
-        } else {
+        }
+        else {
             Write-Host "(it does not)"
         }
     }
@@ -248,25 +263,29 @@ rmdir CyberCNSAgentV2 /s /q
     if (Test-Path $uninstallBatPath) {
         Write-Host "Uninstall batch script exists. Running it..."
         $UninstallReturnValue = Start-Process -FilePath $uninstallBatPath -Wait
-        if(Test-Path $uninstallBatPath) {
+        if (Test-Path $uninstallBatPath) {
             Write-Host "Uninstall batch script still exists after removal attempt, you may wish to verify or manually remove."
         }
         Write-Host "Uninstall batch script removal attempt completed."
         If ( (Get-Service $ServiceNameV3 -ErrorAction SilentlyContinue) ) {
             Write-Host "The service" $ServiceNameV3 "is still installed."
-        } else {
+        }
+        else {
             Write-Host "The service" $ServiceNameV3 "is confirmed to not exist."
         }
-    } elseif ($Force) {
+    }
+    elseif ($Force) {
         Write-Host "Uninstall batch script does not exist and -Force flag is set. Running alternative cleanup script..."
         $UninstallReturnValue = Start-Process -FilePath "cmd.exe" -ArgumentList "/c $alternativeScript" -Wait
         Write-Host "Alternative cleanup script removal attempt completed."
         If ( (Get-Service $ServiceNameV3 -ErrorAction SilentlyContinue) ) {
             Write-Host "The service" $ServiceNameV3 "is still installed."
-        } else {
+        }
+        else {
             Write-Host "The service" $ServiceNameV3 "is confirmed to not exist."
         }
-    } else {
+    }
+    else {
         Write-Host "Uninstall batch script does not exist, nothing else done. To try built-in removal anyway, re-run adding -Force flag."
     }
 
@@ -285,41 +304,59 @@ If ( !$Force -and (Get-Service $ServiceName -ErrorAction SilentlyContinue) ) {
 }
 
 # If no script variable was passed, check Ninja Documentation for the CompanyID
-if(!$CompanyID) {
+if (!$CompanyID -or $CompanyID -eq 0) {
     if ($(Get-Command $CheckNinjaCommand -ErrorAction SilentlyContinue).Name -like $CheckNinjaCommand -and -not [string]::IsNullOrEmpty($customCompanyID) -and -not [string]::IsNullOrWhiteSpace($customCompanyID)) {
         Write-Host "Attempting to get Documentation Custom Field $customCompanyID from the $CustomNinjaDocumentation document."
         $CompanyID = Ninja-Property-Docs-Get-Single $CustomNinjaDocumentation $customCompanyID
     }
 }
 
-# If Ninja Documentation did not return a value, check Ninja Custom Fields
-if (!$CompanyID){
-    Write-Host "Ninja Documentation value not found. Trying Ninja Custom Field."
+# If Ninja Documentation did not return a value, check Ninja Custom Fields for the CompanyID
+if (!$CompanyID -or $CompanyID -eq 0) {
+    Write-Host " Ninja Documentation value not found for CompanyID. Trying Ninja Custom Field."
     $CompanyID = (Ninja-Property-Get $customCompanyID)
+}
+
+# If no script variable was passed, check Ninja Documentation for the User Secret
+if ([string]::IsNullOrEmpty($userSecret) -or [string]::IsNullOrWhiteSpace($userSecret)) {
+    if ($(Get-Command $CheckNinjaCommand -ErrorAction SilentlyContinue).Name -like $CheckNinjaCommand -and -not [string]::IsNullOrEmpty($customUserSecret) -and -not [string]::IsNullOrWhiteSpace($customUserSecret)) {
+        Write-Host "Attempting to get Documentation Custom Field $customUserSecret from the $CustomNinjaDocumentation document."
+        $userSecret = Ninja-Property-Docs-Get-Single $CustomNinjaDocumentation $customUserSecret
+    }
+}
+
+# If Ninja Documentation did not return a value, check Ninja Custom Fields for the User Secret
+if ([string]::IsNullOrEmpty($userSecret) -or [string]::IsNullOrWhiteSpace($userSecret)) {
+    Write-Host " Ninja Documentation value not found gpt User Secret. Trying Ninja Custom Field."
+    $userSecret = (Ninja-Property-Get $customUserSecret)
 }
 
 # if(!$Uninstall) {
 Write-Host "Company ID from $customCompanyID Custom Doc Field, Ninja Custom Field, or passed to script: $CompanyID"
 
 if ($CompanyID -is [int] -and $CompanyID -ge 100 -and $CompanyID -le 999999) {
-    write-host "Company ID passed basic format validation..."
-} else {
-    write-host "No Company ID value provided or invalid format, correctly set via script field, arguement, or custom field."
-    write-host "Format should be: ### (3 or 4 integer digits)"
+    Write-Host "Company ID passed basic format validation..."
+}
+else {
+    Write-Host "No Company ID value provided or invalid format, correctly set via script field, arguement, or custom field."
+    Write-Host "Format should be: ##### (3 to 5 integer digits)"
     exit 1
 }
 if ($TenantID -Match "^[a-f0-9]{18}$") {
-    write-host "Tenant ID passed basic format validation..."
-} else {
-    write-host "No Tenant ID value provided or invalid format, correctly set via script config, arguement, or custom field."
-    write-host "Format should be: ################## (18 decimal numbers)"
+    Write-Host "Tenant ID passed basic format validation..."
+}
+else {
+    Write-Host "No Tenant ID value provided or invalid format, correctly set via script config, arguement, or custom field."
+    Write-Host "Format should be: ################## (18 decimal numbers)"
     exit 1
 }
-if ($userSecret -ne "") {
-    Write-Host "User Secret exists and is not null..."
-} else {
-    Write-Host "No User secret provided. This new field is mandatory."
+if ([string]::IsNullOrEmpty($userSecret) -or [string]::IsNullOrWhiteSpace($userSecret)) {
+    Write-Host "No User secret provided. This new field is mandatory from the -j installer parameter to identify the user generating the installer."
     exit 1
+}
+else {
+    Write-Host "User Secret exists and is not null..."
+    
 }
 
 # Ensure a secure TLS version is used.
@@ -347,10 +384,11 @@ else {
 }
 $ProgressPreference = 'SilentlyContinue'
 
-if($Once) {
+if ($Once) {
     Write-Host "Running one-time Windows device scan without installing agent."
     Write-Host "Scan once not implemented yet for v4."
     exit 10
+    # 
     # $destination = "$InstallLocation\$InstallFilename.exe"
     # if (Test-Path $destination) {
     #     Remove-Item $destination -Force
@@ -360,19 +398,21 @@ if($Once) {
     # $exitCode = [Diagnostics.Process]::Start($destination,"-c $CompanyID -a $ClientID -s $ClientSecret -b $BaseURL -e $CompanyName -m Scan").WaitForExit(3600)
     # # Set destination file to be deleted on computer reboot (when it'll be done being used)
     # New-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name "PendingFileRenameOperations" -Value $($((Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -ErrorAction SilentlyContinue).PendingFileRenameOperations) + "\??\$destination`0`0") -type MultiString -Force | Out-Null
-} else {
+}
+else {
     Write-Host "Installation proceeding"
 }
 
 # Install Lightweight Agent EXE
-$source = (Invoke-RestMethod -Method "Get" -URI "https://configuration.myconnectsecure.com/api/v4/configuration/agentlink?ostype=windows")
+$source = (Invoke-RestMethod -Method "Get" -Uri "https://configuration.myconnectsecure.com/api/v4/configuration/agentlink?ostype=windows")
 $destination = "$InstallLocation\$InstallFilename.exe"
 Invoke-WebRequest -Uri $source -OutFile $destination
-$exitCode = [Diagnostics.Process]::Start($destination,"-c $CompanyID -e $TenantID -j $userSecret -i").WaitForExit(600)
-if($exitCode -eq 0) {
+$exitCode = [Diagnostics.Process]::Start($destination, "-c $CompanyID -e $TenantID -j $userSecret -i").WaitForExit(600)
+if ($exitCode -eq 0) {
     Write-Host "Installation was initiated."
-} else {
+}
+else {
     Write-Host "Installation failed. Check URL and make sure file exists. Exit code: $exitCode"
 }
 # Set destination file to be deleted on computer reboot (when it'll be done being used)
-New-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name "PendingFileRenameOperations" -Value $($((Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -ErrorAction SilentlyContinue).PendingFileRenameOperations) + "\??\$destination`0`0") -type MultiString -Force | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name "PendingFileRenameOperations" -Value $($((Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -ErrorAction SilentlyContinue).PendingFileRenameOperations) + "\??\$destination`0`0") -type MultiString -Force | Out-Null
